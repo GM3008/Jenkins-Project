c56d7e85a6ff93ddcae4812e2a2c31a252bb4633
goutham <goutham@gmail.com>
2022-08-18 14:05:34 +0530

    jenkins

---
+++ gitpush.java
@@ -0,0 +1,16 @@
+public class gitpush
+{
+
+public static void main(String[] args) 
+{
+// TODO Auto-generated method stub
+System.out.println("Hello World");
+System.out.println("Hello Mr mani");
+System.out.println("Hello Mr srini");
+System.out.println("Hello Mr akash");
+System.out.println("Hello Mr Shike");
+System.out.println("Hello Mr allen");
+System.out.println("Hello Mr Gabi");
+System.out.println("Hello Mr ajith");
+}
+}
